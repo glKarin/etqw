f98bcb11d1a7a063127aca955e309d04d8dca61b
Zhao <beyondk2000@gmail.com>
2026-08-25 17:54:58 +0800

    q4xbase

--- idlib/precompiled.h
+++ idlib/precompiled.h
@@ -306,6 +306,8 @@ const float MAX_BOUND_SIZE = 65536.0f;
 #else
     #ifdef _HARDQORE
         #include "../mod/quake4/hardqore/Game.h"
+    #elif defined(_Q4XBASE)
+        #include "../mod/quake4/q4xbase/Game.h"
     #else
         #include "../quake4/Game.h"
     #endif
@@ -372,6 +374,8 @@ const float MAX_BOUND_SIZE = 65536.0f;
 #else
     #ifdef _HARDQORE
         #include "../mod/quake4/hardqore/Game_local.h"
+    #elif defined(_Q4XBASE)
+        #include "../mod/quake4/q4xbase/Game_local.h"
     #else
         #include "../quake4/Game_local.h"
     #endif
